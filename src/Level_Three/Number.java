7dfa8fdddd903b40685e4a60c85c44f331780349
idkimjunyng <kimjunyng@icloud.com>
2025-03-04 17:20:23 +0900

    LEVEL별 패키지 분류
    LEVEL1, LEVEL2 개발 완료
    LEVEL3 개발 미완료

---
+++ src/Level_Three/Number.java
@@ -0,0 +1,7 @@
+package Level_Three;
+
+public enum Number {
+    FIRST_NUMBER,
+    SECOND_NUMBER,
+    RESULT_NUMBER,
+}
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("계산기 과제 중 레벨 1, 레벨 2, 레벨 3 을 선택해 주세요.");
+
     }
 }
